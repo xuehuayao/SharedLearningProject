abfe384d6d09efe55c0d56ae251192ebfdfdb993
yaoxuehua <xuehuayao@leedian.com>
2016-11-23 14:48:57 +0800

    创建空白项目

---
+++ app/src/main/java/com/example/yaoxuehua/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.yaoxuehua.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 音乐播放器主页面
+ * */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
